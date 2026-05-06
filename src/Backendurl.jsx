bc811e7013d9480caddd92612bbc2a435de5cce8
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-05-06 19:22:45 +0530

    Smart city

--- src/Backendurl.jsx
+++ src/Backendurl.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Backendurl = {
-    BASE_URL: "https://smartcityissuebackend-crg5.onrender.com"
+    BASE_URL: "https://smartcityissuebackend-1.onrender.com"
 };
      
 export default Backendurl
